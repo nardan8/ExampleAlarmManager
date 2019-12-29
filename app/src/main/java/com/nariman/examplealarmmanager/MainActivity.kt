67e19083f3aee951e2983fca7f60f53bc0878f8f
Daniyarov Nariman <daniyarov1988@gmail.com>
2019-12-29 13:02:09 +0600

    Initial commit

---
+++ app/src/main/java/com/nariman/examplealarmmanager/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.nariman.examplealarmmanager
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.app.TimePickerDialog
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TimePicker
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
+
+    private var alarmManager: AlarmManager? = null
+    private lateinit var pendingIntent: PendingIntent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun showTimerPickerFragment(view: View) {
+        val timePickerFragment = TimePickerFragment()
+        timePickerFragment.show(supportFragmentManager, "time_picker")
+    }
+
+    fun cancelAlarm(view: View) {
+
+    }
+
+    override fun onTimeSet(timePicker: TimePicker?, hour: Int, minute: Int) {
+
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, hour)
+        calendar.set(Calendar.MINUTE, minute)
+        calendar.set(Calendar.SECOND, 0)
+
+        updateTimeText(calendar)
+        startAlarm(calendar)
+    }
+
+    private fun startAlarm(calendar: Calendar) {
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+    }
+
+    private fun updateTimeText(calendar: Calendar) {
+        val str = "Alarm set for: " + DateFormat.getTimeInstance(DateFormat.SHORT).format(calendar.time)
+        alarm_time_text.text = str
+    }
+}
